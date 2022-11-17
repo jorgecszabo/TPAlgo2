d26d9baff1886dd413e8d59290fcd7a013df598f
Julieta Pages <jpages@dc.uba.ar>
2022-11-17 18:44:48 -0300

    a ver ahora

--- src/Fachada_Variante.cpp
+++ src/Fachada_Variante.cpp
@@ -4,40 +4,25 @@ Fachada_Variante::Fachada_Variante(
         Nat tamanoTab,
         Nat cantFichas,
         const map<Letra, Nat> &puntajes,
-        const set<vector<Letra>> &palabrasLegitimas)  {
-
-    _puntaje = vector<Nat>(TAMANIO_ALFABETO, 0);
-    for(int ord = 0; ord < TAMANIO_ALFABETO; ord++) {
-        Letra letra = inversaDeOrd(ord);
-        if(puntajes.count(letra) == 1){
-            const Nat algo = puntajes[letra];
-            _puntaje[ord] = algo;
-        } else {
-            _puntaje[ord] = 1;
-        }
-    }
-    _palLegitimas = new string_map(TAMANIO_ALFABETO);
-    for (Palabra p : palabrasLegitimas)
-        _palLegitimas->agregar(p);
-    _variante = new Variante(_puntaje, tamanoTab, cantFichas , *_palLegitimas);
-}
+        const set<vector<Letra>> &palabrasLegitimas) : _variante(convertirPuntaje(puntajes),tamanoTab,
+                                                       cantFichas,convertirDiccionario(palabrasLegitimas)) {}
 
 Nat Fachada_Variante::tamanoTablero() const {
-    return _variante->tamTablero();
+    return _variante.tamanoTablero();
 }
 
 Nat Fachada_Variante::fichas() const {
-    return _variante->fichas();
+    return _variante.fichas();
 }
 
 Nat Fachada_Variante::puntajeLetra(Letra l) const {
-    return _variante->puntajeLetra(l);
+    return _variante.puntajeLetra(l);
 }
 
 bool Fachada_Variante::palabraLegitima(const Palabra& palabra) const {
-    return _variante->palLegitima(palabra);
+    return _variante.palabraLegitima(palabra);
 }
 
-const Variante* Fachada_Variante::nuestraVariante() const {
+const Variante& Fachada_Variante::nuestraVariante() const {
     return _variante;
 }
--- src/Fachada_Variante.h
+++ src/Fachada_Variante.h
@@ -15,48 +15,38 @@ public:
             Nat cantFichas,
             const map<Letra, Nat>& puntajes,
             const set<vector<Letra>>& palabrasLegitimas
-    ): _variante(convertirPuntaje(puntajes), tamanoTab, cantFichas,convertirDiccionario(palabrasLegitimas)) {};
+    );
     /**
      * Retorna el tamano del tablero de la variante
      */
-    Nat tamanoTablero() const {
-        return _variante.tamanoTablero();
-    };
+    Nat tamanoTablero() const;
 
     /**
      * Retorna la cantidad de fichas que cada jugador deberia tener en su mano.
      */
-    Nat fichas() const {
-        return _variante.fichas();
-        };
+    Nat fichas() const;
 
     /**
      * Retorna el puntaje de la letra l
      */
-    Nat puntajeLetra(Letra l) const {
-        return _variante.puntajeLetra(l);
-    };
+    Nat puntajeLetra(Letra l) const;
 
     /**
      * Indica si una palabra es legitima o no
      */
-    bool palabraLegitima(const Palabra& palabra) const {
-        return _variante.palabraLegitima(palabra);
-    };
+    bool palabraLegitima(const Palabra& palabra) const;
 
     /**
     * Retorna un puntero a la variante que implementamos, usada para que
     * podamos usar nuestra variante en Juego.h
     */
-     const Variante& nuestraVariante() const {
-        return _variante;
-     }
+     const Variante& nuestraVariante() const;
 
 private:
     Variante _variante;
 
     vector<Nat> convertirPuntaje(const map<Letra, Nat>& puntajes) {
-        vector<Nat> res(TAMANIO_ALFABETO, 0);
+        vector<Nat> res(TAMANIO_ALFABETO, 1);
         for (auto e : puntajes) {
             res[ord(e.first)] = e.second;
         }
